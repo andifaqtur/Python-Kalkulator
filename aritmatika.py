e33544f9d82a2afc825c4c7ae71b38673b20621d
rickiavenged <45161627+rickiavenged@users.noreply.github.com>
2018-11-24 12:18:40 +0700

    pembuatan fungsi operasi dasar

---
+++ aritmatika.py
@@ -0,0 +1,16 @@
+class aritmatika():
+    x=0
+    y=0
+    def __init__ (self):                            #judul menu
+        print("===============================")
+        print("====  K A L K U L A T O R  ====")
+        print("====      Aritmatika       ====")
+        print("=============================== \n")    
+    def tambah(self,x,y):                           #fungsi tambah
+        return x+y
+    def kurang(self,x,y):                           #fungsi pengurangan
+        return x-y
+    def kali(self,x,y):                             #fungsi perkalian
+        return x*y
+    def bagi(self,x, y):                            #fungsi pembagian
+        return x/y
