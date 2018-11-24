d91ff237ba2ee0d25d85b62816ed8506a62b34cf
rickiavenged <45161627+rickiavenged@users.noreply.github.com>
2018-11-24 12:20:38 +0700

    penambahan kelas turunan

--- bangunRuang.py
+++ bangunRuang.py
@@ -1,81 +1,47 @@
-class rumus :
-    def menu(self):
-        print '''
-        =====BANGUN RUANG=====
-        ======================
-        1.LINGKARAN
-        2.PERSEGI
-        3.PERSEGI PANJANG
-        4.BALOK
-        ======================
-        '''
-        pil = input('Masukan Pilihan Anda : ')
-        print '\n'
-        if (pil==1):
-            print '=======LINGKARAN======'
-            bangun.lingkaran()
-            bangun.ulang()
-            
-        elif (pil==2):
-            print '=======PERSEGI======='
-            bangun.persegi()
-            bangun.ulang()
-        
-        elif (pil==3):
-            print '=======PERSEGI PANJANG======='
-            bangun.persegi panjang()
-            bangun.ulang()
-        
-        elif (pil==4):
-            print '=======BALOK======='
-            bangun.balok()
-            bangun.ulang()
-        
-        else :
-            print 'Pilihan tidak tersedia'
-            
-    def ulang(self):
-        print 'n/==================================='
-        ulang = raw_input('apakah anda ingin mencoba lagi [Y/N] : ')
-        print '=====================================/n'
-        if (ulang=='Y' or ulang=='N'):
-            bangun.menu()
-        elif (ulang=='n' or ulang=='N'):
-            print 'Thank you !'
-            quit()
-        else :
-            print 'pilih [Y/N]'
-            bangun.ulang()
-            
-    def Lingkaran(self):
-        r = float(input('Masukan Nilai Jari-jari : '))
-        LuasL = 3.14*r*r
-        print 'Luas Lingkaran dengan jari-jari',r,'adalah'luasL
+class luasPermukaan():           #kelas Induk
 
-    def Persegi(self):
-        s = input('Masukan Nilai sisi Persegi : '))
-        LuasP = s*s
-        print 'Luas dari persegi dengan sisi',s,'adalah'LuasP
+    def luasKubus(self,s):
+        return 6*s*s
 
-    def Persegi Panjang(self):
-        P = input('Masukan Nilai Panjang : ')
-        l = input('Masukan Nilai Lebar : ')
-        LuasPP =P*l
-        print 'Luas dari persegi panjang',p,'dan lebar',1,'adalah',luasPP
- 
-    def Balok(self):
-        p = input('Masukan Nilai Panjang  : ')
-        l = input('Masukan Nilai Lebar    : ')
-        t = input('Masukan Nilai Tinggi   : ')
-        LuasB = p*l*t
-        print 'Nilai Balok dengan Panjang'
-        print ',lebar' ,1,
-        print ',tinggi' ,t,
-        print 'adalah' ,luasB
+    def luasBalok(self,p,l,t):
+        return 2*(p*l)+2*(p*t)+2*(l*t)
 
-    Bangun = rumus()
-    bangun.menu()
-    bangun.lingkaran()
-    bangun.persegi()
-    bangun.persegi panjang()
-    bangun.balok()
+    def luasTabung(self,r,t):
+        return 2*3.14*(r*r)+2*3.14*r*t
+
+    def luasKerucut(self,r,t):
+        return 3.14*r*(r+t)
+                    
+
+class Volume(luasPermukaan): #Kelas turunan
+    def __init__(self):
+        print("===============================")
+        print("====  K A L K U L A T O R  ====")
+        print("====     Bangun Ruang      ====")
+        print("=============================== \n")
+    def volumeKubus(self,s):
+        return s*s*s
+        
+    def volumeBalok(self,p,l,t):
+        return p*l*t
+        
+    def volumeTabung(self,r,t):
+        return 3.14*r*r*t
+
+    def volumeKerucut(self,r,t):
+        return 3.14*r*r*t*1/3
+        
+        
+def menuBangunRuang():
+    print("===============================")
+    print("====  K A L K U L A T O R  ====")
+    print("====     Bangun Ruang      ====")
+    print("===============================")
+    print("[1] Kubus \n[2] Balok \n[3] Tabung \n[4] Kerucut \n[5] <<Kembali")
+    pilih=int(input("Pilihan :"))
+    return pilih
+
+
+        
+    
+        
